3a46f84ad8fee22f88b0f5fcb5dab3c5a53f2f63
dhaminisj <dhaminisj@robosoftin.com>
2023-01-04 16:04:59 +0530

    commit

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const app = require("express")();
-
-app.listen(3000, () => {
-    console.log("litening to 3000");
+const express = require("express");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("welcome");
+});
+app.listen(port, () => {
+    console.log("Server is up on port..");
 });
 
-console.log("in dev dhaminisj");
+app.get("/", (req, res) => {
+    res.send("welcome");
+});
