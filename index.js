0dd113bb0cf9a3e8b996167e2f86094ffe495abf
Sumukh-Robosoft <sumukh.r@robosoftin.com>
2023-01-05 09:33:41 +0530

    otp

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-require("dotenv").config();
-const port = process.env.PORT || 3000;
 const app = express();
+const mongoose = require("mongoose");
+const loginRouter = require("./routes/login");
+require("dotenv").config();
 app.use(express.json());
+app.use("/api/v1", loginRouter);
 app.get("/", (req, res) => {
-    res.send("welcome");
+  res.send("Welcome to laundry app.............");
 });
-app.listen(port, () => {
-    console.log("Server is up on port..");
+
+mongoose.set("strictQuery", true);
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log("connected to DB");
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`running on port ${process.env.PORT}...`);
 });
---
+++ otp.js
@@ -0,0 +1,92 @@
+const nodemailer = require("nodemailer");
+const { totp } = require("otplib");
+
+const sendOtpMail = async (req, res) => {
+  totp.options = { digits: 6, algorithm: "sha512", step: 16660 };
+  const otp = totp.generate(process.env.SECRET_OTP);
+  const transporter = nodemailer.createTransport({
+    service: "zohomail",
+    auth: {
+      user: process.env.ZOHO_MAIL,
+      pass: process.env.ZOHO_PASS,
+    },
+    port: 465,
+    host: "smtp.zoho.in",
+    secure: true,
+  });
+  const mailOptions = {
+    from: process.env.ZOHO_MAIL,
+    to: req.body.email,
+    subject: "laundry OTP",
+    text: `laundry Otp is ${otp}`,
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      res.send({
+        message: error,
+      });
+    } else {
+      res.json({
+        message: "OTP sent successfully",
+      });
+    }
+  });
+};
+
+const verifyOtpMail = async (req, res) => {
+  try {
+    const isValid = totp.check(req.body.otp, process.env.SECRET_OTP);
+    res.json({
+      message: isValid,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+const sendOtpPhone = async (req, res) => {
+  try {
+    totp.options = { digits: 4, algorithm: "sha512", step: 16660 };
+
+    const otp = totp.generate(process.env.SECRET_OTP);
+
+    const dest = req.body.destination;
+
+    const nexmo = new Nexmo({
+      apiKey: process.env.VONAGE_API_KEY,
+      apiSecret: process.env.VONAGE_API_SECRET,
+    });
+
+    const from = "laundry Otp verification";
+    const to = `${dest}`;
+    const text = `HEllo from laundry, this is your otp for verification is ${otp} `;
+
+    const x = await nexmo.message.sendSms(
+      "+919481676348",
+      Number(dest),
+      text,
+      (err, response) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(response);
+        }
+      }
+    );
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+const verifyOtpPhone = async (req, res) => {
+  try {
+    const isValid = totp.check(req.body.otp, process.env.SECRET_OTP);
+    res.json({
+      message: isValid,
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+};
+
+module.exports = { sendOtpMail, verifyOtpMail, verifyOtpPhone, sendOtpPhone };
