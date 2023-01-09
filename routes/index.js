3b016eb00b583d5f4f89c5f0d03f4e5545325ba4
Keerthika <keerthika@robosoftin.com>
2023-01-09 11:55:26 +0530

    created database for user

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/UserController");
+
+const upload = require("../../multer");
+
+module.exports = router;
