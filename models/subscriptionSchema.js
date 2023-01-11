f660cdd98ad57dd5a10d9624e470fe9677ef5c8e
Sumukh-Robosoft <sumukh.r@robosoftin.com>
2023-01-11 14:45:40 +0530

    buy subscription

---
+++ models/subscriptionSchema.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const subscriptionSchema = mongoose.Schema({
+    userId:{
+        type:String
+    },
+    pickupDays:[{
+        pickupDays:[],
+        deliveryDays:[]
+    }],
+    amount:{
+        type:Number
+    },
+    months:{
+        type:Number
+    },
+    numberOfPickups:{
+        type:Number
+    },
+    isPaused:{
+        type:Boolean,
+        default:false
+    },
+    ifPaused:[{
+        from:{
+            type:Date
+        },
+        to:{
+            type:Date
+        }
+    }],
+    subscriptionStart:{
+        type:Date
+    },
+    subscriptionEnd:{
+        type:Date
+    },
+    deliveryType:{
+        type:String
+    },
+    deliverySlot:{
+        type:String
+    },
+    address:{
+        title:{
+            type:String
+        },
+        address:{
+            type:String
+        },
+        district:{
+            type:String
+        },
+        pincode:{
+            type:String
+        }
+    }
+})
+
+module.exports = mongoose.model("buySubscription",subscriptionSchema)
