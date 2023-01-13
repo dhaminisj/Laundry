520edd481326a54755a6b7b0731772d9624a1be4
Sameeksha M V <sameeksha.mv@robosoftin.com>
2023-01-13 12:26:39 +0530

    Laundry list schema

--- models/laundryListSchema.js
+++ models/laundryListSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { single } = require("../utils/multer");
 
 const laundryListSchema = mongoose.Schema({
   type: {
@@ -22,16 +23,70 @@ const laundryListSchema = mongoose.Schema({
   image: {
     type: String,
   },
-
   singlePack: {
     type: Number,
     default: 8,
   },
-  hanger: { type: Number, default: 10 },
-  multiplePack: { type: Number, default: 20 },
-
-  starch: { type: Number, default: 8 },
-  noStrach: { type: Number, default: 10 },
+  singlePackDescription: {
+    type: String,
+    default:
+      "Want each piece packed single, not ready to mingle? No, that's not unreasonable. To each garment, its own! Items are packed individually, with a cardboard support and in a white paper bag.",
+  },
+  singlePackUrl: {
+    type: String,
+    default: "https://www.youtube.com/watch?v=CPUKWMPyOvA",
+  },
+  hanger: {
+    type: Number,
+    default: 10,
+  },
+  hangerDescription: {
+    type: String,
+    default:
+      "No creases, no folds? Why not let them hang out uncrushed till you waer them? We pack your garment on a hanger, with a compostable outer cover.",
+  },
+  hangerUrl: {
+    type: String,
+    default: "https://www.youtube.com/watch?v=GmHgusGNoec",
+  },
+  multiplePack: {
+    type: Number,
+    default: 0,
+  },
+  multiPackDescription: {
+    type: String,
+    default:
+      "Pack it together, stack it together, you say. Okay. After all, you know your cupboard or suitcase better. Your garments come neatly packed and primed, in a brown paper bag.",
+  },
+  multiPackUrl: {
+    type: String,
+    default: "https://www.youtube.com/watch?v=jF3dgfXEgB8",
+  },
+  starch: {
+    type: Number,
+    default: 8,
+  },
+  starchDescription: {
+    type: String,
+    default: "The process for making clothes wrinkle-free longer",
+  },
+  StarchUrl: {
+    type: String,
+    default: "https://www.youtube.com/watch?v=GI9Uer-r8cg",
+  },
+  noStrach: {
+    type: Number,
+    default: 10,
+  },
+  noStarchDescription: {
+    type: String,
+    default:
+      "It just means that starch is not used in the process of steaming or ironing clothes.",
+  },
+  noStarchUrl: {
+    type: String,
+    default: "https://www.youtube.com/watch?v=QsyPECwiolw",
+  },
 });
 
 module.exports = mongoose.model("laundryList", laundryListSchema);
