21219970af6be81abf668b0cfba8f429382e5ea1
Sameeksha M V <sameeksha.mv@robosoftin.com>
2023-01-17 13:01:19 +0530

    updateratings  fixed in ratingController

--- controller/ratingController.js
+++ controller/ratingController.js
@@ -3,64 +3,43 @@ const User = require("../models/UserSchema");
 const ratingsModel = require("../models/ratingsSchema");
 
 const addRating = async (req, res) => {
-    const { userId } = req.users;
-    const { star, subscriptionId } = req.body;
-    try {
-        //const subscription = await subscriptionList.findById(subscriptionId);
-        let alreadyRated = await ratingsModel.findOne({
-            subscriptionId,
-            ratings: { $elemMatch: { postedby: userId } },
-        });
-        if (alreadyRated) {
-            const updateRating = await ratingsModel.findOneAndUpdate(
-                {
-                    subscriptionId,
-                    ratings: { $elemMatch: { postedby: userId } },
-                },
-                {
-                    $set: {
-                        "ratings.$.star": star,
-                    },
-                }
-            );
-            res.status(200).json({
-                message: "Rating Updated Successfully",
-            });
-        } else {
-            // const rate = await ratingsModel.findOneAndUpdate(
-            //     { subscriptionId },
-            //     {
-            //         $push: {
-            //             ratings: {
-            //                 star: star,
-            //                 postedby: userId,
-            //             },
-            //         },
-            //     },
-            //     {
-            //         new: true,
-            //     }
-            // );
-            const rate = new ratingsModel({
-                subscriptionId,
-
-                // ratings: [
-                //     {
-                star: star,
-                postedby: userId,
-                //     },
-                // ],
-            });
-            await rate.save();
-            res.status(200).json({
-                message: "Rating Added Succesfully",
-            });
+  const { userId } = req.users;
+  const { star, subscriptionId } = req.body;
+  try {
+    let alreadyRated = await ratingsModel.findOne({
+      subscriptionId,
+      ratings: { $elemMatch: { postedby: userId } },
+    });
+    if (alreadyRated) {
+      const updateRating = await ratingsModel.findOneAndUpdate(
+        {
+          subscriptionId,
+          ratings: { $elemMatch: { postedby: userId } },
+        },
+        {
+          $set: {
+            star,
+          },
         }
-    } catch (error) {
-        // console.log(error);
-        res.status(400).json({ message: error.message });
+      );
+      res.status(200).json({
+        message: "Rating Updated Successfully",
+      });
+    } else {
+      const rate = new ratingsModel({
+        subscriptionId,
+        star: star,
+        postedby: userId,
+      });
+      await rate.save();
+      res.status(200).json({
+        message: "Rating Added Succesfully",
+      });
     }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 module.exports = {
-    addRating,
+  addRating,
 };
