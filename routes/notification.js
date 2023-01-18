4a0bda88b433866d1273f8d0f58cca558bc943bf
Sameeksha M V <sameeksha.mv@robosoftin.com>
2023-01-18 12:29:21 +0530

    changed route to post

--- routes/notification.js
+++ routes/notification.js
@@ -5,5 +5,5 @@ const notificationController = require("../controller/notificationConroller");
 const { getNotifications } = require("../controller/notificationConroller");
 
 router.route("/add-notification").post(notificationController.addNotifications);
-router.route("/get-notification").get(verifyJWT, getNotifications);
+router.route("/get-notification").post(verifyJWT, getNotifications);
 module.exports = router;
