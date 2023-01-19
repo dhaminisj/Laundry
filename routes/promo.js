40ae8b2df497d85c4b4f979a6055b91206eadbc0
Sameeksha M V <sameeksha.mv@robosoftin.com>
2023-01-19 15:36:26 +0530

    Promo Code done

--- routes/promo.js
+++ routes/promo.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const verifyJWT = require("../middleware/verifyJWT");
+const upload = require("../utils/multer");
 const promoController = require("../controller/promoController");
 
-router.route("/add-promo-code").post(promoController.addPromoCode);
+router
+  .route("/add-promo-code")
+  .post(upload.single("image"), promoController.addPromoCode);
 router.route("/get-promo-code").post(verifyJWT, promoController.getPromoCode);
 
 module.exports = router;
