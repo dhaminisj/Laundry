f5ad856c95df9d3ea5e6cba32c03268eaaf9735c
keerthika06 <111507657+keerthika06@users.noreply.github.com>
2023-01-24 12:25:19 +0530

    get profile

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controller/UserController");
 const ratingController = require("../controller/ratingController");
+const refreshController = require("../controller/refreshController")
 const upload = require("../utils/multer");
 const checkUserLoggedIn = require("../middleware/checkUserIsLoggedIn");
 const verifyJWT = require("../middleware/verifyJWT");
@@ -18,6 +19,8 @@ router
   .post(upload.single("image"), verifyJWT, userController.updateUserProfilePic)
   .get(verifyJWT, userController.getProfile);
 router.route("/edit-profile").post(verifyJWT, userController.editProfile);
+
+router.route("/handle-refresh").get(refreshController.handleRefreshToken)
 
 router.route("/add-address").post(verifyJWT, userController.addAddress);
 router
