fed8f243fdfc38a10d5fb9ed458270e833ba5add
Sameeksha M V <sameeksha.mv@robosoftin.com>
2023-01-31 11:04:04 +0530

    Get details by phone - userController

--- otp.js
+++ otp.js
@@ -3,25 +3,25 @@ const { totp } = require("otplib");
 const Nexmo = require("nexmo");
 
 const sendOtpMail = async (req, res) => {
-    totp.options = { digits: 6, algorithm: "sha512", step: 16660 };
-    const otp = totp.generate(process.env.SECRET_OTP);
+  totp.options = { digits: 6, algorithm: "sha512", step: 16660 };
+  const otp = totp.generate(process.env.SECRET_OTP);
 
-    const transporter = nodemailer.createTransport({
-        service: "zohomail",
-        auth: {
-            user: process.env.ZOHO_MAIL,
-            pass: process.env.ZOHO_PASS,
-        },
-        port: 465,
-        host: "smtp.zoho.in",
-        secure: true,
-    });
-    const mailOptions = {
-        from: process.env.ZOHO_MAIL,
-        to: req.body.email,
-        subject: "laundry OTP",
-        text: `laundry Otp is ${otp}`,
-    };
+  const transporter = nodemailer.createTransport({
+    service: "zohomail",
+    auth: {
+      user: process.env.ZOHO_MAIL,
+      pass: process.env.ZOHO_PASS,
+    },
+    port: 465,
+    host: "smtp.zoho.in",
+    secure: true,
+  });
+  const mailOptions = {
+    from: process.env.ZOHO_MAIL,
+    to: req.body.email,
+    subject: "laundry OTP",
+    text: `laundry Otp is ${otp}`,
+  };
 
   transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
@@ -35,66 +35,66 @@ const sendOtpMail = async (req, res) => {
 };
 
 const verifyOtpMail = async (req, res) => {
-    try {
-        const isValid = totp.check(req.body.otp, process.env.SECRET_OTP);
-        if (isValid) {
-            res.status(200).json({ statusCode: 200, message: "otp valid" });
-        } else {
-            res.status(401).json({ statusCode: 401, message: "otp invalid" });
-        }
-    } catch (error) {
-        res.status(500).json({ statusCode: 500, errorMessage: error.message });
+  try {
+    const isValid = totp.check(req.body.otp, process.env.SECRET_OTP);
+    if (isValid) {
+      res.status(200).json({ statusCode: 200, message: "otp valid" });
+    } else {
+      res.status(401).json({ statusCode: 401, message: "otp invalid" });
     }
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, errorMessage: error.message });
+  }
 };
 
 const sendOtpPhone = async (req, res) => {
-    try {
-        totp.options = { digits: 4, algorithm: "sha512", step: 16660 };
+  try {
+    totp.options = { digits: 4, algorithm: "sha512", step: 16660 };
 
-        const otp = totp.generate(process.env.SECRET_OTP);
+    const otp = totp.generate(process.env.SECRET_OTP);
 
-        const dest = req.body.destination;
+    const dest = req.body.destination;
 
-        const nexmo = new Nexmo({
-            apiKey: process.env.VONAGE_API_KEY,
-            apiSecret: process.env.VONAGE_API_SECRET,
-        });
+    const nexmo = new Nexmo({
+      apiKey: process.env.VONAGE_API_KEY,
+      apiSecret: process.env.VONAGE_API_SECRET,
+    });
 
-        const from = "laundry Otp verification";
-        const to = `${dest}`;
-        const text = `HEllo from laundry, this is your otp for verification is ${otp} `;
+    const from = "laundry Otp verification";
+    const to = `91${dest}`;
+    const text = `HEllo from laundry, this is your otp for verification is ${otp} `;
 
-        const x = await nexmo.message.sendSms(
-            "+919481676348",
-            Number(dest),
-            text,
-            (err, response) => {
-                if (err) {
-                    res.status(502).json({
-                        statusCode: 502,
-                        message: "Couldn't send OTP",
-                    });
-                } else {
-                    res.status(200).json({ statusCode: 200, response });
-                }
-            }
-        );
-    } catch (error) {
-        res.status(500).json({ statusCode: 500, errorMessage: error.message });
-    }
+    const x = await nexmo.message.sendSms(
+      "+919481676348",
+      Number(dest),
+      text,
+      (err, response) => {
+        if (err) {
+          res.status(502).json({
+            statusCode: 502,
+            message: "Couldn't send OTP",
+          });
+        } else {
+          res.status(200).json({ statusCode: 200, response });
+        }
+      }
+    );
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, errorMessage: error.message });
+  }
 };
 
 const verifyOtpPhone = async (req, res) => {
-    try {
-        const isValid = totp.check(req.body.otp, process.env.SECRET_OTP);
-        if (isValid) {
-            res.status(200).json({ statusCode: 200, message: "otp valid" });
-        } else {
-            res.status(401).json({ statusCode: 401, message: "otp invalid" });
-        }
-    } catch (error) {
-        res.status(500).json({ statusCode: 500, errorMessage: error.message });
+  try {
+    const isValid = totp.check(req.body.otp, process.env.SECRET_OTP);
+    if (isValid) {
+      res.status(200).json({ statusCode: 200, message: "otp valid" });
+    } else {
+      res.status(401).json({ statusCode: 401, message: "otp invalid" });
     }
+  } catch (error) {
+    res.status(500).json({ statusCode: 500, errorMessage: error.message });
+  }
 };
 
 module.exports = { sendOtpMail, verifyOtpMail, verifyOtpPhone, sendOtpPhone };
