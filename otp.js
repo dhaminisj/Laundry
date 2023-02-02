c022af4f2f48bf4b83a2e6273780538d617caed3
Sumukh-Robosoft <sumukh.r@robosoftin.com>
2023-02-02 13:46:09 +0530

    otp updates

--- otp.js
+++ otp.js
@@ -1,6 +1,7 @@
 const nodemailer = require("nodemailer");
 const { totp } = require("otplib");
 const Nexmo = require("nexmo");
+const User = require("./models/UserSchema");
 
 const sendOtpMail = async (req, res) => {
   totp.options = { digits: 6, algorithm: "sha512", step: 16660 };
@@ -67,8 +68,8 @@ const sendOtpPhoneRegister = async (req, res) => {
     const text = `HEllo from laundry, this is your otp for verification is 123456 `;
     if (!userfound) {
       const x = await nexmo.message.sendSms(
-        "+919481676348",
-        Number(dest),
+        "+919448745406",
+        Number(to),
         text,
         (err, response) => {
           if (err) {
@@ -110,7 +111,7 @@ const sendOtpPhoneLogin = async (req, res) => {
     if (userfound) {
       const x = await nexmo.message.sendSms(
         "+919481676348",
-        Number(dest),
+        Number(to),
         text,
         (err, response) => {
           if (err) {
